40a7fefd121410956999d03ebf2b9ab1cfb1067d
nova3333 <stephenkielb26@gmail.com>
2020-11-18 12:57:50 -0500

    Cleaned Up projects.js / project.html

--- projects.js
+++ projects.js
@@ -1,33 +1,9 @@
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyBW2lFWZ7kcCYQYue2bn3GwJ__3o2dRN4A",
-    authDomain: "testproject-e2e7b.firebaseapp.com",
-    databaseURL: "https://testproject-e2e7b.firebaseio.com/",
-    projectId: "testproject-e2e7b",
-    storageBucket: "testproject-e2e7b.appspot.com",
-    messagingSenderId: "235711990032",
-    appId: "1:235711990032:web:c351938147d72dd6983d95",
-    measurementId: "G-WDMH8VHBE6"
-	
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-// Your web app's Firebase configuration
-  var firebaseConfig = {
-//firebase config stuff
-  };
-
-  
-	const auth = firebase.auth();
 	
 var storage = firebase.storage();
 var storageRef = storage.ref();
 // var listRef = storageRef.child('Bellevue Project/documents') this should list all files how the fuck DO I LIST THE DIRECTORIES
-var listRef = storageRef.child('Bellevue Project/documents');
+var listRef = storageRef.child("Harrowford Project/documents");
 function myfunction(){
   listRef.listAll().then(function(res) {
     res.prefixes.forEach(function(folderRef) {
@@ -38,6 +14,7 @@ function myfunction(){
     count = 0;
     res.items.forEach(function(itemRef) {
       document.write(itemRef);
+	  document.write("<br>");
       // All the items under listRef.
     });
   }).catch(function(error) {
