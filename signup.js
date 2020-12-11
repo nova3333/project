e0b93934458db6ff6a3b33adc7d5c2d825652871
Ray O <71047193+cheems-supreme@users.noreply.github.com>
2020-12-11 15:21:06 +0000

    Add files via upload
    Added code to handle the insertion of user information (name, first and last, and email) to Firebase Firestore (prevents general errors with app, that use this information.

--- signup.js
+++ signup.js
@@ -1,14 +1,26 @@
+function signUp(){
+	var db = firebase.firestore();
 
-	function signUp(){
-		
-		var email = document.getElementById("email");
-		var password = document.getElementById("password");
-		
-		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-	
-				
-			promise.then(() => alert("Signed Up")).catch(e => alert(e.message));
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	var firstNameVal = document.getElementById("firstName").value;
+	var lastNameVal = document.getElementById("lastName").value;
+
+	const promise = auth.createUserWithEmailAndPassword(email.value, password.value)
+		.then(() => {
+			db.collection("users").add({
+				"email":email.value,
+				"first_name":firstNameVal,
+				"last_name":lastNameVal
+			})
+			.then(function(docRef) {
+				console.log("Document ID written with: ", docRef.id);
+			})
+			.catch(function(error) {
+				console.log("Error with adding document: ", error);
+			})
+		}).catch(e => alert(e.message));
+
 
 	
-	}
-	
+}	
